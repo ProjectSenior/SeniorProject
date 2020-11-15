99c2d2ac7b70ea04b69a844c5f8ba7c43f11426a
kunlachart promfang <kunlachart.p@ku.th>
2020-11-16 02:08:19 +0700

    add iotinfo

--- app/src/main/java/com/example/seniorproject/PlantInfo.kt
+++ app/src/main/java/com/example/seniorproject/PlantInfo.kt
@@ -3,8 +3,6 @@ package com.example.seniorproject
 class PlantInfo {
     var temp = 0
     var plantName = ""
-    var pHSoil = 0
-    var moisture = 0
     var date = ""
     var status = 0
     var anno = ""
