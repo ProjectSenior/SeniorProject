90e59e9b647a901b62dcc6f42b310c72b80755a1
kunlachart promfang <kunlachart.p@ku.th>
2020-11-17 05:02:23 +0700

    edit Text in XML

--- app/src/main/java/com/example/seniorproject/IotInfo.kt
+++ app/src/main/java/com/example/seniorproject/IotInfo.kt
@@ -1,15 +1,17 @@
 package com.example.seniorproject
 
+import android.app.DatePickerDialog
+
 class IotInfo {
     var temp = ""
     var humidity = ""
     var moisture = ""
-    var currentTimelimit = ""
-    constructor(temp: String, moisture: String, humidity: String,currentTimelimit:String) {
+    var Date = ""
+    constructor(temp: String, moisture: String, humidity: String,Date:String) {
         this.temp = temp
         this.moisture = moisture
         this.humidity = humidity
-        this.currentTimelimit = currentTimelimit
+        this.Date = Date
     }
 
 
